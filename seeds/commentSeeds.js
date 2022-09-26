361e9aa305eaa95a91c7d91a0b0edbfc184831df
Josh Man <joshuamangerino@gmail.com>
2022-09-26 16:04:40 -0600

    updated seeds

--- seeds/commentSeeds.js
+++ seeds/commentSeeds.js
@@ -2,11 +2,25 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
-        user_id: 1,
-        post_id: 1,
-        comment_text: "test"
+        comment_text: "Testing",
+        user_id: 2,
+        post_id: 1
     },
+    {
+        comment_text: "Still Testing",
+        user_id: 3,
+        post_id: 1
+    },
+    {
+        comment_text: "Hopefully this works",
+        user_id: 1,
+        post_id: 1
+    }
 ]
+
+
+
+
 
 const seedComments = () => Comment.bulkCreate(commentData);
 
