57c944633ad9eab37739f0c0ac55cdb129f692c2
Josh Man <joshuamangerino@gmail.com>
2022-09-26 15:51:04 -0600

    trying to fix server issues

--- utils/auth.js
+++ utils/auth.js
@@ -1,10 +1,11 @@
 const withAuth = (req, res, next) => {
-    if (!req.session.logged_in) {
+  console.log(req.session)
+    if (!req.session.loggedIn) {
       res.redirect('/login');
-    } else {
+    } 
+    else {
       next();
     }
   };
   
   module.exports = withAuth;
-  
