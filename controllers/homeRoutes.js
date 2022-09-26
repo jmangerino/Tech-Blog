0b2dcbdfa5003eadd8ac105bc76a4c46ac316aba
Josh Man <joshuamangerino@gmail.com>
2022-09-26 16:16:08 -0600

    updating routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,68 +1,93 @@
 const router = require('express').Router();
-const { Post, Comment, User } = require('../models/');
+const { Project, User, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-// get all posts for homepage
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll({
-      include: [User],
-    });
-    
-    const posts = postData.map((post) => post.get({ plain: true }));
-
-    res.render('all-posts-admin', { posts, loggedIn: req.session.loggedIn});
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// get single post
-router.get('/post/:id', withAuth, async (req, res) => {
-  try {
-    
-    const postData = await Post.findOne({
-      
-      where: {id: req.params.id},
+    // Get all projects and JOIN with user data
+    const projectData = await Project.findAll({
       include: [
-        User,
         {
-          model: Comment,
-          include: [User],
+          model: User,
+          attributes: ['name'],
         },
       ],
     });
 
-    if (postData) {
-      
-      const post = postData.get({ plain: true });
-     
-      console.log(post);
-      res.render('single-post', { post, loggedIn: req.session.loggedIn});
-    } else {
-      res.status(404).end();
-    }
+    // Serialize data so the template can read it
+    const projects = projectData.map((project) => project.get({ plain: true })).reverse();
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      projects, 
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+router.get('/project/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ['name'],
+            }
+          ]
+        }
+      ],
+    });
 
-router.get('/login', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/dashboard');
-    return;
+    const project = projectData.get({ plain: true });
+    console.log(project)
+    // console.log(project.comments[0].user)
+
+    res.render('test', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
-  res.render('login');
 });
 
-router.get('/signup', (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect('/dashboard');
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Project }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
     return;
   }
 
-  res.render('signup');
+  res.render('login');
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
-const dashboardRoutes = require('./dashboardRoutes');
+
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 
-
-//middleware for routes
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
 
 module.exports = router;
