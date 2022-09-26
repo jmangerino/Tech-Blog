7247b666aa3ee326b3359c7bb65a7b00e58cf422
Josh Man <joshuamangerino@gmail.com>
2022-09-26 16:14:17 -0600

    updating files

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -2,18 +2,19 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
-    const body = req.body;
-  
-    try {
-      const newComment = await Comment.create({
-        ...body,
-        userId: req.session.userId,
-      });
-      res.json(newComment);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
+router.post('/', async (req, res) => {
 
-  module.exports = router;
+    try {
+        const newComment = await Comment.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        });
+        console.log(newComment)
+
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
--- seeds/userSeeds.js
+++ seeds/userSeeds.js
@@ -1,38 +1,24 @@
 const { User } = require('../models');
 
-const userData = [
-    {
-        "username": "datapirate",
-        "email": "matt@gmail.com",
-        "password": "pass123",
-        "bio": "UC Berkeley Coding Bootcamp student, aspiring to be a web developer!"
-    },
-    {
-        "username": "codebreaker",
-        "email": "abby@gmail.com",
-        "password": "pass456",
-        "bio": "Entry level web developer specializing in JavaScript. Looking to network with other beginner devs!"
-    },
-    {
-        "username": "infiniteloop",
-        "email": "eddie@gmail.com",
-        "password": "pass789",
-        "bio": "Based in the SF Bay Area. Undergrad in CS."
-    },
-    {
-        "username": "johnDoe",
-        "email": "john@gmail.com",
-        "password": "pass789",
-        "bio": "Into all things dev. Let's connect!"
-    },
-    {
-        "username": "eddieg",
-        "email": "eddie@gmail.com",
-        "password": "pass789",
-        "bio": "Newly graduated coding boot camp alumni. Looking for my next big opportunity!"
-    }
-]
+const userdata =
+[
+  {
+    "username": "Can",
+    "password": "password"
+  },
+  {
+    "username": "Ali",
+    "password": "password"
+  },
+  {
+    "username": "Bono",
+    "password": "password"
+  }
+];
 
-const seedUsers = () => User.bulkCreate(userData);
+const seedUser = () => User.bulkCreate(userdata, {
+  individualHooks: true,
+  returning: true,
+});
 
-module.exports = seedUsers;
+module.exports = seedUser;
